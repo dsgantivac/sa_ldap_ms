39a1142ff43b0eab27b144a28261e446ce40cf23
David Gantiva <dsgantivac@unal.edu.co>
2019-06-23 16:38:44 -0500

    second c ommit

--- ldapPrueba.py
+++ ldapPrueba.py
@@ -13,11 +13,11 @@
 import ldap
 #pip3 install python-ldap
 ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT,0)
-con = ldap.initialize("ldaps://192.168.99.101:636")
+con = ldap.initialize("ldap://192.168.99.101:389")
 con.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
 #
-user_dn = "cn=David Gantiva,dc=arqsoft,dc=unal,dc=edu,dc=co"
-user_password = "1234"
+user_dn = "cn=admin,dc=arqsoft,dc=unal,dc=edu,dc=co"
+user_password = "admin"
 con.simple_bind_s(user_dn,user_password)
 
 
